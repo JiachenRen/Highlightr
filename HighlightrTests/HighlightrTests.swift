732d805d77dfece2c0929197ecb64de7a8efd7c6
jiachenren <arc4jiachenren@gmail.com>
2019-03-03 10:45:40 -0500

    Split highlight(_:_:_:) into formatHtml(_:) and render(_:)

---
+++ HighlightrTests/HighlightrTests.swift
@@ -0,0 +1,123 @@
+//
+//  HighlightrTests.swift
+//  HighlightrTests
+//
+//  Created by Jiachen Ren on 3/3/19.
+//  Copyright © 2019 Illanes, Juan Pablo. All rights reserved.
+//
+
+import XCTest
+import Highlightr
+
+class HighlightrTests: XCTestCase {
+    
+    let swiftCodeSegment =
+"""
+    //
+    //  Scope.swift
+    //  Kelvin
+    //
+    //  Created by Jiachen Ren on 1/21/19.
+    //  Copyright © 2019 Jiachen Ren. All rights reserved.
+    //
+
+    import Foundation
+
+    public struct Scope {
+        
+        /// An array that keeps track of variable definitions and operations.
+        private static var stack = [Scope]()
+        
+        private var definitions: [String: Node]
+        private var operations: [String: [Operation]]
+        private static var restricted = [String: Node]()
+        
+        init(_ definitions: [String: Node], _ operations: [String: [Operation]]) {
+            self.definitions = definitions
+            self.operations = operations
+        }
+        
+        /**
+         Capture and save current scope - that is, all variable definitions,
+         operations, and function definitions.
+         */
+        public static func save() {
+            let curScope = Scope(Variable.definitions, Operation.registered)
+            stack.append(curScope)
+        }
+        
+        /**
+         Restore to default operations, variable & constant definitions.
+         */
+        public static func restoreDefault() {
+            Variable.restoreDefault()
+            Operation.restoreDefault()
+        }
+        
+        /**
+         Pop the last saved scope from the stack and use it as a blueprint
+         to restore function and variable definitions.
+         */
+        public static func restore() {
+            let scope = stack.removeLast()
+            Operation.registered = scope.operations
+            Variable.definitions = scope.definitions
+        }
+        
+        /// Discard last saved scope
+        @discardableResult
+        public static func popLast() -> Scope {
+            return stack.removeLast()
+        }
+        
+        /**
+         Temporarily withhold any attempts to access the variable.
+         */
+        public static func withholdAccess(to vars: Variable...) {
+            vars.forEach {v in
+                let n = v.name
+                if let def = Variable.definitions[n] {
+                    restricted[n] = def
+                    Variable.definitions.removeValue(forKey: n)
+                }
+            }
+        }
+        
+        public static func withholdAccess(to vars: [Variable]) {
+            vars.forEach {withholdAccess(to: $0)}
+        }
+        
+        public static func releaseRestrictions() {
+            for (key, value) in restricted {
+                Variable.define(key, value)
+            }
+            restricted.removeAll()
+        }
+    }
+
+"""
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testHighlight() {
+        guard let highlightr = Highlightr() else {
+            XCTFail("failed to initialize highlightr")
+            return
+        }
+        highlightr.highlight(swiftCodeSegment, as: "swift", fastRender: true)
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
--- Pod/Classes/Highlightr.swift
+++ Pod/Classes/Highlightr.swift
@@ -14,13 +14,10 @@ import JavaScriptCore
 #endif
 
 /// Utility class for generating a highlighted NSAttributedString from a String.
-open class Highlightr
-{
+open class Highlightr {
     /// Returns the current Theme.
-    open var theme : Theme!
-    {
-        didSet
-        {
+    open var theme : Theme! {
+        didSet {
             themeChanged?(theme)
         }
     }
@@ -47,33 +44,28 @@ open class Highlightr
 
      - returns: Highlightr instance.
      */
-    public init?(highlightPath: String? = nil)
-    {
+    public init?(highlightPath: String? = nil) {
         let jsContext = JSContext()!
         let window = JSValue(newObjectIn: jsContext)
         jsContext.setObject(window, forKeyedSubscript: "window" as NSString)
 
         let bundle = Bundle(for: Highlightr.self)
         self.bundle = bundle
-        guard let hgPath = highlightPath ?? bundle.path(forResource: "highlight.min", ofType: "js") else
-        {
+        guard let hgPath = highlightPath ?? bundle.path(forResource: "highlight.min", ofType: "js") else {
             return nil
         }
         
         let hgJs = try! String.init(contentsOfFile: hgPath)
         let value = jsContext.evaluateScript(hgJs)
-        if value?.toBool() != true
-        {
+        if value?.toBool() != true {
             return nil
         }
-        guard let hljs = window?.objectForKeyedSubscript("hljs") else
-        {
+        guard let hljs = window?.objectForKeyedSubscript("hljs") else {
             return nil
         }
         self.hljs = hljs
         
-        guard setTheme(to: "pojoaque") else
-        {
+        guard setTheme(to: "pojoaque") else {
             return nil
         }
         
@@ -87,8 +79,7 @@ open class Highlightr
      - returns: true if it was possible to set the given theme, false otherwise
      */
     @discardableResult
-    open func setTheme(to name: String) -> Bool
-    {
+    open func setTheme(to name: String) -> Bool {
         guard let defTheme = bundle.path(forResource: name+".min", ofType: "css") else
         {
             return false
@@ -101,6 +92,58 @@ open class Highlightr
     }
     
     /**
+     Format the code by enveloping keywords, strings, values, etc., in html tags.
+     
+     - parameter code:           Code to highlight.
+     - parameter languageName:   Language name or alias. Set to `nil` to use auto detection.
+     */
+    open func formatHtml(_ code: String, as languageName: String? = nil) -> String? {
+        let ret: JSValue
+        if let languageName = languageName {
+            ret = hljs.invokeMethod("highlight", withArguments: [languageName, code, ignoreIllegals])
+        } else {
+            // language auto detection
+            ret = hljs.invokeMethod("highlightAuto", withArguments: [code])
+        }
+        
+        let res = ret.objectForKeyedSubscript("value")
+        return res?.toString()
+    }
+    
+    /**
+     Renders the provided html by applying the specified theme.
+     
+     - Parameter html: Formatted html to be rendered
+     - parameter fastRender:     Defaults to true - When *true* will use the custom made html parser rather than Apple's solution.
+     
+     - returns: NSAttributedString with the detected code highlighted.
+     */
+    open func render(_ html: String, fastRender: Bool = true) -> NSAttributedString? {
+        var html = html
+        var returnString : NSAttributedString?
+        if fastRender {
+            returnString = processHTMLString(html)!
+        } else {
+            html = "<style>"+theme.lightTheme+"</style><pre><code class=\"hljs\">"+html+"</code></pre>"
+            let opt: [NSAttributedString.DocumentReadingOptionKey : Any] = [
+                .documentType: NSAttributedString.DocumentType.html,
+                .characterEncoding: String.Encoding.utf8.rawValue
+            ]
+            
+            let data = html.data(using: String.Encoding.utf8)!
+            safeMainSync {
+                returnString = try? NSMutableAttributedString(
+                    data: data,
+                    options: opt,
+                    documentAttributes: nil
+                )
+            }
+        }
+        
+        return returnString
+    }
+    
+    /**
      Takes a String and returns a NSAttributedString with the given language highlighted.
      
      - parameter code:           Code to highlight.
@@ -109,44 +152,11 @@ open class Highlightr
      
      - returns: NSAttributedString with the detected code highlighted.
      */
-    open func highlight(_ code: String, as languageName: String? = nil, fastRender: Bool = true) -> NSAttributedString?
-    {
-        let ret: JSValue
-        if let languageName = languageName
-        {
-            ret = hljs.invokeMethod("highlight", withArguments: [languageName, code, ignoreIllegals])
-        }else
-        {
-            // language auto detection
-            ret = hljs.invokeMethod("highlightAuto", withArguments: [code])
-        }
-
-        let res = ret.objectForKeyedSubscript("value")
-        guard var string = res!.toString() else
-        {
+    open func highlight(_ code: String, as languageName: String? = nil, fastRender: Bool = true) -> NSAttributedString? {
+        guard let htmlStr = formatHtml(code, as: languageName) else {
             return nil
         }
-        
-        var returnString : NSAttributedString?
-        if(fastRender)
-        {
-            returnString = processHTMLString(string)!
-        }else
-        {
-            string = "<style>"+theme.lightTheme+"</style><pre><code class=\"hljs\">"+string+"</code></pre>"
-            let opt: [NSAttributedString.DocumentReadingOptionKey : Any] = [
-             .documentType: NSAttributedString.DocumentType.html,
-             .characterEncoding: String.Encoding.utf8.rawValue
-             ]
-            
-            let data = string.data(using: String.Encoding.utf8)!
-            safeMainSync
-            {
-                returnString = try? NSMutableAttributedString(data:data, options: opt, documentAttributes:nil)
-            }
-        }
-        
-        return returnString
+        return render(htmlStr, fastRender: fastRender)
     }
     
     /**
@@ -154,8 +164,7 @@ open class Highlightr
      
      - returns: Array of Strings
      */
-    open func availableThemes() -> [String]
-    {
+    open func availableThemes() -> [String] {
         let paths = bundle.paths(forResourcesOfType: "css", inDirectory: nil) as [NSString]
         var result = [String]()
         for path in paths {
@@ -170,8 +179,7 @@ open class Highlightr
      
      - returns: Array of Strings
      */
-    open func supportedLanguages() -> [String]
-    {
+    open func supportedLanguages() -> [String] {
         let res = hljs.invokeMethod("listLanguages", withArguments: [])
         return res!.toArray() as! [String]
     }
@@ -179,32 +187,27 @@ open class Highlightr
     /**
      Execute the provided block in the main thread synchronously.
      */
-    private func safeMainSync(_ block: @escaping ()->())
-    {
-        if Thread.isMainThread
-        {
+    private func safeMainSync(_ block: @escaping ()->()) {
+        if Thread.isMainThread {
             block()
-        }else
-        {
-            DispatchQueue.main.sync { block() }
+        } else {
+            DispatchQueue.main.sync {
+                block()
+            }
         }
     }
     
-    private func processHTMLString(_ string: String) -> NSAttributedString?
-    {
+    private func processHTMLString(_ string: String) -> NSAttributedString? {
         let scanner = Scanner(string: string)
         scanner.charactersToBeSkipped = nil
         var scannedString: NSString?
         let resultString = NSMutableAttributedString(string: "")
         var propStack = ["hljs"]
         
-        while !scanner.isAtEnd
-        {
+        while !scanner.isAtEnd {
             var ended = false
-            if scanner.scanUpTo(htmlStart, into: &scannedString)
-            {
-                if scanner.isAtEnd
-                {
+            if scanner.scanUpTo(htmlStart, into: &scannedString) {
+                if scanner.isAtEnd {
                     ended = true
                 }
             }
@@ -212,8 +215,7 @@ open class Highlightr
             if scannedString != nil && scannedString!.length > 0 {
                 let attrScannedString = theme.applyStyleToString(scannedString! as String, styleList: propStack)
                 resultString.append(attrScannedString)
-                if ended
-                {
+                if ended {
                     continue
                 }
             }
@@ -222,19 +224,15 @@ open class Highlightr
             
             let string = scanner.string as NSString
             let nextChar = string.substring(with: NSMakeRange(scanner.scanLocation, 1))
-            if(nextChar == "s")
-            {
+            if nextChar == "s" {
                 scanner.scanLocation += (spanStart as NSString).length
                 scanner.scanUpTo(spanStartClose, into:&scannedString)
                 scanner.scanLocation += (spanStartClose as NSString).length
                 propStack.append(scannedString! as String)
-            }
-            else if(nextChar == "/")
-            {
+            } else if nextChar == "/" {
                 scanner.scanLocation += (spanEnd as NSString).length
                 propStack.removeLast()
-            }else
-            {
+            } else {
                 let attrScannedString = theme.applyStyleToString("<", styleList: propStack)
                 resultString.append(attrScannedString)
                 scanner.scanLocation += 1
@@ -243,21 +241,19 @@ open class Highlightr
             scannedString = nil
         }
         
-        let results = htmlEscape.matches(in: resultString.string,
-                                               options: [.reportCompletion],
-                                               range: NSMakeRange(0, resultString.length))
+        let results = htmlEscape.matches(
+            in: resultString.string,
+            options: [.reportCompletion],
+            range: NSMakeRange(0, resultString.length)
+        )
         var locOffset = 0
-        for result in results
-        {
+        for result in results {
             let fixedRange = NSMakeRange(result.range.location-locOffset, result.range.length)
             let entity = (resultString.string as NSString).substring(with: fixedRange)
-            if let decodedEntity = HTMLUtils.decode(entity)
-            {
+            if let decodedEntity = HTMLUtils.decode(entity) {
                 resultString.replaceCharacters(in: fixedRange, with: String(decodedEntity))
                 locOffset += result.range.length-1;
             }
-            
-
         }
 
         return resultString
