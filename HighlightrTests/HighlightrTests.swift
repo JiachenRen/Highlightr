7dc59546d430fcf738c610a835e9c9681091a074
jiachenren <arc4jiachenren@gmail.com>
2019-03-03 15:12:13 -0500

    Syntax highlighting support for Kelvin!

--- HighlightrTests/HighlightrTests.swift
+++ HighlightrTests/HighlightrTests.swift
@@ -11,90 +11,19 @@ import Highlightr
 
 class HighlightrTests: XCTestCase {
     
-    let swiftCodeSegment =
+    let kelvinCodeSegment =
 """
-    //
-    //  Scope.swift
-    //  Kelvin
-    //
-    //  Created by Jiachen Ren on 1/21/19.
-    //  Copyright © 2019 Jiachen Ren. All rights reserved.
-    //
-
-    import Foundation
-
-    public struct Scope {
-        
-        /// An array that keeps track of variable definitions and operations.
-        private static var stack = [Scope]()
-        
-        private var definitions: [String: Node]
-        private var operations: [String: [Operation]]
-        private static var restricted = [String: Node]()
-        
-        init(_ definitions: [String: Node], _ operations: [String: [Operation]]) {
-            self.definitions = definitions
-            self.operations = operations
-        }
-        
-        /**
-         Capture and save current scope - that is, all variable definitions,
-         operations, and function definitions.
-         */
-        public static func save() {
-            let curScope = Scope(Variable.definitions, Operation.registered)
-            stack.append(curScope)
-        }
-        
-        /**
-         Restore to default operations, variable & constant definitions.
-         */
-        public static func restoreDefault() {
-            Variable.restoreDefault()
-            Operation.restoreDefault()
-        }
-        
-        /**
-         Pop the last saved scope from the stack and use it as a blueprint
-         to restore function and variable definitions.
-         */
-        public static func restore() {
-            let scope = stack.removeLast()
-            Operation.registered = scope.operations
-            Variable.definitions = scope.definitions
-        }
-        
-        /// Discard last saved scope
-        @discardableResult
-        public static func popLast() -> Scope {
-            return stack.removeLast()
-        }
-        
-        /**
-         Temporarily withhold any attempts to access the variable.
-         */
-        public static func withholdAccess(to vars: Variable...) {
-            vars.forEach {v in
-                let n = v.name
-                if let def = Variable.definitions[n] {
-                    restricted[n] = def
-                    Variable.definitions.removeValue(forKey: n)
-                }
-            }
-        }
-        
-        public static func withholdAccess(to vars: [Variable]) {
-            vars.forEach {withholdAccess(to: $0)}
-        }
-        
-        public static func releaseRestrictions() {
-            for (key, value) in restricted {
-                Variable.define(key, value)
-            }
-            restricted.removeAll()
-        }
+def f(x) {
+    if (x > 3) {
+        println factor(x)
     }
-
+    exit()
+    while (true) {
+        x := 3.14159
+        "sdfsdf" as string
+    }
+}
+f(x^2+log(x)^2*x)
 """
     let highlightr = Highlightr()!
 
@@ -107,7 +36,7 @@ class HighlightrTests: XCTestCase {
     }
     
     func getFormatedHtml() -> String {
-        guard let html = highlightr.formatHtml(swiftCodeSegment, as: "swift") else {
+        guard let html = highlightr.formatHtml(kelvinCodeSegment, as: "kelvin") else {
             XCTFail("failed to format code as html")
             fatalError()
         }
@@ -128,7 +57,7 @@ class HighlightrTests: XCTestCase {
     
     func testPerformanceFastRender() {
         let html = getFormatedHtml()
-        
+        print(html)
         // Test fast render performance
         self.measure {
             guard let _ = highlightr.render(html, fastRender: true) else {
